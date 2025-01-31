7894e50fdbfb69b002c7f0fc0327c4d61ad38d73
matias <xgalladex1234@gmail.com>
2025-01-30 23:41:21 -0300

    delete front

--- frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import AppRouter from "./AppRouter";
-
-export default function App() {
-  return (
-    <>
-      <AppRouter />
-    </>
-  );
-}
--- frontend/src/AppRouter.jsx
+++
@@ -1,6 +0,0 @@
-import { Routes, Route } from "react-router-dom";
-
-
-const AppRouter = () => {
-    return <div> hola</div>
-}
